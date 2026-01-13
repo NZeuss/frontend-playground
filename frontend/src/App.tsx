179aef4cfea87009da38b50cd1d9d1bdf1aecfa5
bschuedzig <benjamin.schuedzig@mantro.net>
2026-01-13 13:33:35 +0100

    enh: adds tsconfig paths; prepares for shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-
 function App() {
 
   return (
-    <div className="bg-red-500">
-      Hello World
+    <div className="h-screen flex flex-row">
+      <div className="bg-red-500 w-96">test</div>
+      <div className="bg-green-500 flex-1">test</div>
     </div>
   );
 }
